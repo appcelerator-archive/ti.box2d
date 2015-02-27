5e44f37cf847f511a3e49acd5ca80b1e1c54fb5c
Ingo Muschenetz <ingo@Grace.local>
2015-02-26 20:36:07 -0800

    Add build output directory info for xctool

--- ios/build.py
+++ ios/build.py
@@ -149,10 +149,10 @@ def glob_libfiles():
 
 def build_module(manifest,config):
 	if(cmd_exists("xctool") == True):
-		rc = os.system("xctool -sdk iphoneos -configuration Release -scheme box2d")
+		rc = os.system("xctool -sdk iphoneos -configuration Release -scheme box2d CONFIGURATION_BUILD_DIR=\"build/Release-iphonesimulator/\"")
 		if rc != 0:
 			die("xctool failed")
-		rc = os.system("xctool -sdk iphonesimulator -configuration Release -scheme box2d")
+		rc = os.system("xctool -sdk iphonesimulator -configuration Release -scheme box2d CONFIGURATION_BUILD_DIR=\"build/Release-iphonesimulator/\"")
 		if rc != 0:
 			die("xctool failed")
 	else:
